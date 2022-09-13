a3edbe9fdc820adeee46c16e08816566b27946be
Kevin <US.Kevin@iCloud.com>
2022-09-12 17:06:47 -0700

    fixed styling, removed gender expression, and added localized header

--- angular-i18n-plugin/src/app/app.component.ts
+++ angular-i18n-plugin/src/app/app.component.ts
@@ -7,21 +7,20 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-i18n-plugin';
+  title = 'Receipt';
 
   constructor(private titleService: Title) {
     this.titleService.setTitle($localize`${this.title}`);
   }
 
+  header: string = $localize`Ticket Confirmation`;
+
   receipt = {
-      minutesUpdated: 2,
-      gender: 'male',
+      minutesUpdated: 1,
       miles: 10000,
       purchaseDate: "01/01/2023",
       cityTax: .086,
       currency: 1529.22,
-    }
-    // 
-  ;
+    };
 
 }
